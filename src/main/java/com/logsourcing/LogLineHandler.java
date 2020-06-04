566ae1e8b06a65d816c28eba184c79dc81bc8909
Shimon Magal <shimon.magal@gmail.com>
2020-06-04 16:32:11 +0300

    atlc

--- src/main/java/com/logsourcing/LogLineHandler.java
+++ src/main/java/com/logsourcing/LogLineHandler.java
@@ -1,5 +1,15 @@
 package com.logsourcing;
 
 // Takes in a log line, and decomposes it to time, severity, and log-pattern
+
 public class LogLineHandler {
+    private String[] COMMON_REGEX_TO_STRIP = {"d+", ".*\\\\.*", ".*/.*"};
+
+    // expected to be given in the form of groups
+    private final String lineGroupRegex;
+
+    LogLineHandler(String lineGroupRegex)
+    {
+        this.lineGroupRegex = lineGroupRegex;
+    }
 }
