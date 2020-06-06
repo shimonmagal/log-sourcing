ebcad7e89306c5e2e05d318f278e903b89427760
Shimon Magal <shimon.magal@gmail.com>
2020-06-06 23:32:33 +0300

    log line

--- src/main/java/com/logsourcing/LogLine.java
+++ src/main/java/com/logsourcing/LogLine.java
@@ -1,2 +1,15 @@
-package com.logsourcing;public class LogLine {
+package com.logsourcing;
+
+public class LogLine {
+    String message;
+    Severity severity;
+    long time;
+
+    private enum Severity {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        FATAL
+    }
 }
