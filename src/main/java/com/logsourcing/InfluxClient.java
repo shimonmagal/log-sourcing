3e863ffa6177ab6dda6b3a43ffd73123407e62dc
Shimon Magal <shimon.magal@gmail.com>
2020-06-12 21:47:39 +0300

    Influx

--- src/main/java/com/logsourcing/InfluxClient.java
+++ src/main/java/com/logsourcing/InfluxClient.java
@@ -1,5 +1,14 @@
 package com.logsourcing;
 
-public class InfluxClient {
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.internal.InfluxDBClientImpl;
 
+public class InfluxClient {
+    private final InfluxDBClient influx;
+
+    public InfluxClient()
+    {
+        this.influx = new InfluxDBClientImpl(d);
+        this.influx = new Infl
+    }
 }
