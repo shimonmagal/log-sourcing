5d1596e4717fe437447b03da5b3f76209fc78de4
Shimon Magal <shimon.magal@gmail.com>
2020-08-07 07:32:04 +0300

    define the Influx client writer

--- src/main/java/com/logsourcing/InfluxClient.java
+++ src/main/java/com/logsourcing/InfluxClient.java
@@ -20,6 +20,9 @@ public class InfluxClient {
         p.addTag("SEVERITY", line.severity.name());
         p.time(line.time, WritePrecision.MS);
         p.addTag("PATTERN", line.messagePattern);
+        p.addTag("MACHINE", line.machineName);
+        p.addTag("APP", line.appName);
+        p.addTag("VERSION", line.versionName);
 
         influxDBClient.getWriteApi().writePoint(p);
     }
